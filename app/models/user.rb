bab18d4eed11b8368fe2be541cf08acea7510f48
nicguy59 <mowen@northislandcollege.ca>
2015-03-31 01:21:14 +0000

    Clearance tested, adding views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   has_many :payfwds
   has_many :shippings
 
+  # user = User.find(1)
+  # user.record_sign_in
+  #def record_sign_in
+	#update!(last_signed_in_at: Time.now)
+	# send a text message?
+	# update their points?
+	# notify the NSA?
+  #end
+
 end
