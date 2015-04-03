9357284df5f63b209fb3e48c78e9ac29b914e58f
nicguy59 <mowen@northislandcollege.ca>
2015-04-03 07:13:45 +0000

    More CSS, fiddling with Orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,15 @@
 class OrdersController < ApplicationController
+  helper_method :current_member
+
+  def index
+    @order = Order.find(params[:id])
+  end
+
   def show
     @order = Order.find(params[:id])
+  end
+
+  def current_member
+    @_member ||= Member.find(params[:member_id])
   end
 end
